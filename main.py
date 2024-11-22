795c3eeff28c18ddb70cc0930924a87663f82271
N-Temple16 <nigelatemple@gmail.com>
2024-11-22 12:47:34 -0500

    created a bookbot program

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = book_text(book_path)
+    num_words = word_count(text)
+    num_chars = char_count(text)
+    char_list = [{"letter": k, "count": v} for k, v in num_chars.items()]
+    char_list.sort(reverse=True, key=sort_on)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")    
+    for char in char_list:
+        print(f"The '{char['letter']}' character was found {char['count']} times")
+    print("--- End report ---")
+
+def sort_on(dict):
+    return dict["count"]
+
+def char_count(text):
+    lowered_text = text.lower()
+    amount = {}
+    for s in set(lowered_text):
+        if s.isalpha():
+            amount[s] = lowered_text.count(s)
+    return amount
+
+def word_count(text):
+    words = text.split()
+    return len(words)
+
+def book_text(path):
+    with open(path) as f:
+        return f.read()
+
 
 main()
